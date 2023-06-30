a51405dc52cf482c3e27d96f311a7c7bf61f411f
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-06-30 14:31:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import paho.mqtt.client as paho
 import time
 from IPython.display import Audio
+import streamlit as st
+
 
 broker="157.230.214.127"
 port=1883
+
+client1=paho.Client("GIT-HUB")
+client1.on_publish = on_publish    
+client1.subscribe("Sensores")
+client1.on_message = on_message
+
+
+
+st.title("MQTT Control")
 def on_publish(client,userdata,result):             #create function for callback
     print("el dato ha sido publicado \n")
     pass
@@ -18,19 +29,32 @@ def on_message(client, userdata, message):
        sound_file = 'hum_high.mp3'
        display(Audio(sound_file, autoplay=True))
 
-client1=paho.Client("Python")
-client1.on_publish = on_publish    
-client1.subscribe("Sensores")
-client1.on_message = on_message
-client1.connect(broker,port)
+
+if st.button('Enviar msg'):
+    client1= paho.Client("GIT-HUB")                           
+    client1.on_publish = on_publish                          
+    client1.connect(broker,port)                                 
+    ret= client1.publish("deteccion","Taza")                   
+    
+else:
+    st.write('')
+
+values = st.slider(
+    'Selecciona el rango de valores',
+    0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
 
 
-while True:                          
+
+
+
+
+#while True:                          
                                                                
     #client1.publish("seleccion","plastico")                   
-    time.sleep(0.5)
-    client1.loop_start()
-    client1.subscribe('Sensores')
-    #time.sleep(2)
-    #client1.loop_stop();
+ #   time.sleep(0.5)
+ #   client1.loop_start()
+ #   client1.subscribe('Sensores')
+ #   #time.sleep(2)
+ #   #client1.loop_stop();
   
